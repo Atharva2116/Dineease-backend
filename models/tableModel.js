0a9874ee61cf9af7a8a0b866dc3d429e88409089
Atharva Dandgawhal <156278775+Atharva2116@users.noreply.github.com>
2025-08-04 01:25:55 +0530

    Update tableModel.js

--- models/tableModel.js
+++ models/tableModel.js
@@ -1,12 +1,15 @@
 import mongoose from "mongoose";
 
-const tableSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  capacity: { type: Number, required: true },
-  isBooked: { type: Boolean, default: false },
-  time: { type: String, default: null },
-  duration: { type: String, default: null },
-});
+const tableSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    capacity: { type: Number, required: true },
+    isBooked: { type: Boolean, default: false },
+    time: { type: String, default: null },
+    duration: { type: String, default: null },
+  },
+  { collection: "tables" } // 👈 Add this line explicitly
+);
 
 const Table = mongoose.model("Table", tableSchema);
 export default Table;
